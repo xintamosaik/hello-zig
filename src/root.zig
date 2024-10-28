ae86d6fa1126ac4c12b521565d82212893a829d7
Ulf Dellbrügge <xintamosaik@gmail.com>
2024-10-28 20:21:13 +0100

    revert to init base

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
+const testing = std.testing;
 
-pub fn main() void {
-    std.debug.print("Hello, World!\n", .{});
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
 }
